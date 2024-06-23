7c7838da52da3942266e93e1aeee2098c9c68c4d
Osada <mezzoforte.mackerel@icloud.com>
2024-06-23 17:03:43 +0900

    mod画面の表示を行う部分まで作成

---
+++ src/main/java/com/osadsakana/battlesoundscapes/BattleSoundscapes.java
@@ -0,0 +1,15 @@
+package com.osadsakana.battlesoundscapes;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(BattleSoundscapes.MODID)
+public class BattleSoundscapes {
+
+  public static final String MODID = "battlesoundscapes";
+  public static final String MODNAME = "Battle Soundscapes";
+  public static final String MODVERSION = "0.0.1";
+
+  public BattleSoundscapes() {
+    // 
+  }
+}
